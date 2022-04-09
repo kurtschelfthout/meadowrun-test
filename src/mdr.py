3997435c9048f35262c24d0667a9eba7d1078e34
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2022-04-09 21:22:55 +0100

    Print the result

--- src/mdr.py
+++ src/mdr.py
@@ -3,7 +3,7 @@ from meadowrun import run_function, EC2AllocHost, Deployment
 from test import my_cool_function
 
 async def main():
-    await run_function(
+    result = await run_function(
         my_cool_function,
         EC2AllocHost(
             logical_cpu_required=1,
@@ -11,11 +11,13 @@ async def main():
             interruption_probability_threshold=15),
         Deployment.git_repo(
             "https://github.com/kurtschelfthout/meadowrun-test",
-            conda_yml_file="env.yml",
+            conda_yml_file="../env.yml",
             branch="master",
-            commit="master"
+            commit="master",
+            path_in_repo="src"
         )
     )
+    print(f"It worked, got {result=}")
 
 if __name__ == "__main__":
     asyncio.run(main())
